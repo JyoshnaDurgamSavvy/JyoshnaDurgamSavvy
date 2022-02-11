543f38f10d7b91782d4afe66a1570ce69de8ad87
JyoshnaDurgamSavvy <99382696+JyoshnaDurgamSavvy@users.noreply.github.com>
2022-02-11 17:22:49 +0530

    Add files via upload

---
+++ program to print Fibonacci numbers to given limit.py
@@ -0,0 +1,18 @@
+terms = int(input("Enter the terms"))
+a = 0
+b = 1
+count = 0
+if(items<=0):
+	print("Please enter a valid integer")
+elif(terms == 1):
+	print("Fibonacci sequence upto",limit,":")
+	print(a)
+else:
+	print("Fibonacci sequence:")
+	while (count<terms):
+		print(a,end='')
+		c=a+b
+		a=b
+		b=c
+		count+=1
+		
